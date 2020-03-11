f61050424c00ae522035ea3b0746e3446f342e17
sandip mali <Sandip.Mali@Emerson.com>
2020-03-11 14:40:01 +0530

    added startup for DI

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿
+using LoadAtimoDataToDW;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace LoadAtimoDataToDW
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
